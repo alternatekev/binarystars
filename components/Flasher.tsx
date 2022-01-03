1d45ba2d545d43ba698e4da84e071aa7f9ebfa34
Kevin Conboy <kevin.conboy@meraki.net>
2022-01-03 15:49:32 -0700

    animating

--- components/Flasher.tsx
+++ components/Flasher.tsx
@@ -1,34 +1,28 @@
 import { css, keyframes } from "@emotion/core";
 import { style } from "@styles";
-import { FC } from "react";
+import { FC, useState } from "react";
 
-const bounce = keyframes`
-  from, 20%, 53%, 80%, to {
-    transform: translate3d(0,0,0);
-  }
+export const Flasher: FC = () => {
+  const styles = style({
+    Flasher: {
+      width: 15,
+      height: 2,
+      display: "inline-block",
+      marginLeft: 5,
+      backgroundColor: "rgba(255, 153, 153, 1)",
+      transition: "none",
+    },
+    animating: {
+      transition: `300ms background-color ease-in-out`,
+      backgroundColor: "rgba(255, 153, 153, 0)",
+    },
+  });
 
-  40%, 43% {
-    transform: translate3d(0, -30px, 0);
-  }
+  const [animating, setAnimating] = useState(true);
 
-  70% {
-    transform: translate3d(0, -15px, 0);
-  }
+  setTimeout(() => {
+    setAnimating(!animating);
+  }, 700);
 
-  90% {
-    transform: translate3d(0,-4px,0);
-  }
-`;
-
-const styles = style({
-  Flasher: {
-    borderBottom: `3px #F99 solid`,
-    width: 20,
-    height: 5,
-    display: "inlineBlock",
-    marginLeft: 5,
-    animation: `${bounce} 1s ease infine`,
-  },
-});
-
-export const Flasher: FC = () => <div css={css(styles.Flasher)} />;
+  return <div css={css(styles.Flasher, animating && styles.animating)} />;
+};
